4e19ff01cc419152ba6eb4860f6173b2704a3d3d
pongdanny <pongdaniel17@gmail.com>
2020-11-10 17:11:51 -0500

    phase 2 complete

--- pet-detail-page-jsx/src/App.js
+++ pet-detail-page-jsx/src/App.js
@@ -1,8 +1,8 @@
+import React from "react";
+import PetDetailPage from "./PetDetailPage";
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  return <PetDetailPage></PetDetailPage>;
 }
 
 export default App;
